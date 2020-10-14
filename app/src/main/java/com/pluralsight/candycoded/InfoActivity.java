56a83784c2e26492173aa7446dffb3f764a43a5e
boluwatife003 <72657732+boluwatife003@users.noreply.github.com>
2020-10-14 18:11:06 +0100

    Completed module

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 
@@ -30,9 +31,21 @@ public class InfoActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    // ***
-    // TODO - Task 2 - Launch the Google Maps Activity
-    // ***
+    public void createMapIntent(View view){
+        Intent intent = new Intent(Intent.ACTION_VIEW).setPackage("com.google.android.apps.maps");
+        Uri uri = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
+        intent.setData(uri);
+
+
+        intent.resolveActivity(getPackageManager());
+        startActivity(intent);
+//        }else{
+//            Toast.makeText(this, "No Map client is available", Toast.LENGTH_SHORT).show();
+//        }
+
+    }
+
+
 
     // ***
     // TODO - Task 3 - Launch the Phone Activity
